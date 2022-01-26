ffaadbe7542e5c4431e0e836f3a21ac17fe79131
Anna <annasarafanova91@gmail.com>
2022-01-25 20:02:52 -0800

    removed token

--- config.js
+++
@@ -1,3 +0,0 @@
-const token = '4244b2e072msh42f226f65bba068p1fb704jsnddffe20c4424';
-
-module.exports = token;
--- server/models.js
+++ server/models.js
@@ -3,7 +3,7 @@ const axios = require('axios');
   const url = 'https://api-nba-v1.p.rapidapi.com';
   const headers =  {
     'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-    'x-rapidapi-key': 'YOUR API KEY'
+    'x-rapidapi-key': ''
   }
 
   module.exports = {
